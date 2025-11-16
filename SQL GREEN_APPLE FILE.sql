6eb6e8054617401b70a1a629e069173086003deb
Shashi Shekhar <uishashi@yahoo.com>
2025-11-16 23:00:17 +0530

    Update SQL GREEN_APPLE FILE.sql

--- SQL GREEN_APPLE FILE.sql
+++ SQL GREEN_APPLE FILE.sql
@@ -51,6 +51,76 @@ from employees
 group by department_name
 order by count(*)desc ;
 
+/* Show top 5 highest-paid employees....................................................................*/
+select employee_id , salary
+from employees
+order by salary desc
+limit 5;
+
+/*Show employees whose phone_number is NULL.*/
+
+select employee_id, phone_number
+from employees
+where phone_number is null;
+
+/*show employees whose name starts with 'N'.*/
+ select  employee_name
+ from employee
+ where employee_name like 'N%';
+ 
+ /*Show all customers who live in ‘Delhi’.*/
+ select customer_name
+ from customers
+ where city like 'delhi%';
+ 
+ /*Show customers older than 40*/
+  select customer_id, age
+ from customers
+  where age < '40' ;
+
+/*Count customers in each city.*/
+select count(customer_id), city
+from customers
+group by city;
+
+/*Show customers whose name ends with '5'*/
+select customer_name
+from customers
+where customer_name like '%5';
+
+/*Show all orders above ₹10,000*/
+select order_id
+from orders
+where order_amount <10000;
+
+/*Show latest 10 orders using ORDER BY order_date.*/
+select order_id, order_date
+from orders
+order by order_date desc
+limit 10;
+
+/*Count how many orders each customer has placed.*/
+select customer_id, count(order_id)
+from orders
+group by customer_id;
+
+/*Show total order_amount (sum) for customer_id = 10.*/
+select customer_id, sum(order_amount)
+from orders
+where customer_id like '%10'
+group by customer_id;
+
+/*Show orders placed between '2022-06-01' and '2022-12-31'.*/
+select order_id , order_date
+from orders
+where order_date between '2022-06-01' and '2022-12-31';
+
+/* Join customers and orders to show customer_name + order_amount.*/
+select c.customer_name, sum(o.order_amount)
+from customers c
+join orders o on o.customer_id = o.customer_id 
+group by c.customer_name ;
+
 
 
 
